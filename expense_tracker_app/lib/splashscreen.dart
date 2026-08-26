49f59346160c0b2e73c472e4e14a890e1c2f7006
rizkyharsa <rizkyharsa2@gmail.com>
2026-08-26 22:08:18 +0700

    create splash screen

---
+++ expense_tracker_app/lib/splashscreen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'modules/home/views/home_view.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  Timer startSplashScreen() {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () async => Get.offAll(() => const HomeView(), transition: Transition.fadeIn));
+  }
+
+  @override
+  void initState() {
+    startSplashScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(alignment: Alignment.center, child: Image.asset("assets/images/logo1.png", scale: 3)),
+    );
+  }
+}
